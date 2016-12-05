b5e685ada68c0272be9ad6b270c8e30582328686
Christian Buco <christianbuco17@gmail.com>
2016-12-05 08:39:52 -1000

    donut

---
+++ arrays_object.js
@@ -0,0 +1,22 @@
+/*
+	Christian Buco
+	Arrays and Objects
+	Dec. 5, 2016
+*/
+
+ // variables
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior Tan", "Zakattak", "Yolo", "Ohai", "Jeff"];
+var colors = ["black", "salmon", "light blue"];
+var adj = ["smart", "special case", "yummy", "tiny whiny", "ferocious"];
+var slang = ["scrap", "beef", "da kine", "la dat", "pau hana"];
+
+// objects
+
+var donut = {
+	topping : ["sprinkles", "choco frosting", "o'hare air"],
+	filling : "jelly",
+	shape : "dodecahedron",
+	count : 12
+}
